bde713a99693ba538a9a8c701dcc60d7a9bb3918
Abrar Mahabub (Nowrid) <abrar.nowrid@gmail.com>
2024-04-09 15:56:35 +0600

    routes primarily created

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,34 @@
-const express = require("express");
-
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
 const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  //root rout of the server https://localhost:5000
+  res.send("hello vividvoices created by Nowrid!");
+});
+
+// authentication //
+
+app.use("/api/auth", authRoutes);
+//signup auth
+app.get("/api/auth/signup", (req, res) => {
+  res.send("signup route");
+});
+
+//login auth
+app.get("/api/auth/login", (req, res) => {
+  res.send("login route");
+});
+
+//logout auth
+app.get("/api/auth/logout", (req, res) => {
+  res.send("logout route");
+});
 
 app.listen(5000, () => {
-  console.log("app is running on port 5000");
+  console.log(`app is running on port ${PORT}`);
 });
