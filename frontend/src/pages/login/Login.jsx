2349a2c7e64dafad5dee81834c8998da19b7926c
Abrar Mahabub (Nowrid) <abrar.nowrid@gmail.com>
2024-04-13 20:07:15 +0600

    login frontend done

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,67 @@
+const Login = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-1g shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          Login 
+          <span className="text-black"> VividVoices</span>
+        </h1>
+        <form >
+          <div>
+          <label className="label p-2">
+            <span className="text-base label-text text-black">Username</span>
+          </label>
+          <input type="text" placeholder="Enter Username" className="w-full input input-bordered h-10" />
+          </div>
+          <div>
+          <label className="label">
+            <span className="text-base label-text text-black">Password</span>
+          </label>
+            <input type="text" placeholder="Enter Password" className="w-full input input-bordered h-10" />
+          </div>
+          <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">{"Don't"} have any account ?</a>
+          
+          <div>
+          <button className="btn btn-block btn-sm mt-2">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
+
+
+// starter code for this file
+/*const Login = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-1g shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          Login 
+          <span className="text-black"> VividVoices</span>
+        </h1>
+        <form >
+          <div>
+          <label className="label p-2">
+            <span className="text-base label-text text-black">Username</span>
+          </label>
+          <input type="text" placeholder="Enter Username" className="w-full input input-bordered h-10" />
+          </div>
+          <div>
+          <label className="label">
+            <span className="text-base label-text text-black">Password</span>
+          </label>
+            <input type="text" placeholder="Enter Password" className="w-full input input-bordered h-10" />
+          </div>
+          <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">{"Don't"} have any account ?</a>
+          
+          <div>
+          <button className="btn btn-block btn-sm mt-2">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+} */
---
+++ frontend/src/pages/signup/SignUp.jsx
@@ -0,0 +1,11 @@
+const SignUp = () => {
+  return (
+      <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+          <div>
+              
+          </div>
+      </div>
+  )
+}
+
+export default SignUp
