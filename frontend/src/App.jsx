73a7b4b529654a1dd8b0afe01b4c8569a0b5cc7f
Abrar Mahabub (Nowrid) <abrar.nowrid@gmail.com>
2024-04-14 12:26:35 +0600

    userSignup.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Home from './pages/home/Home'
 import SignUp from './pages/signup/SignUp'
 import Login from './pages/login/Login'
+import { Toaster } from 'react-hot-toast'
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
@@ -11,6 +12,7 @@ function App() {
         <Route path='/login' element={<Login/>} />
         <Route path='/signup' element={<SignUp/>} />
       </Routes>
+      <Toaster />
       </div>
   )
 }
---
+++ frontend/src/hooks/userSignup.js
@@ -0,0 +1,35 @@
+import toast from "react-hot-toast";
+
+const userSignup = () => {
+  const [loading, setLoading] = useState(false);
+  const signup = async ({
+    fullName,
+    username,
+    password,
+    confirmPassword,
+    gender,
+  }) => {
+    const success = handleInputErrors({
+      fullName,
+      username,
+      password,
+      confirmPassword,
+      gender,
+    });
+    if (!success) return;
+  };
+};
+
+export default userSignup;
+
+function handleInputErrors({
+  fullName,
+  username,
+  password,
+  confirmPassword,
+  gender,
+}) {
+  if (!fullName || !username || !password || !confirmPassword || !gender) {
+    toast.error("Please fill in all fields");
+  }
+}
