86b20a4008afe5b1647aff25e70c880373347eac
Abrar Mahabub (Nowrid) <abrar.nowrid@gmail.com>
2024-04-15 08:39:35 +0600

    updating useSignUp.js

--- frontend/src/hooks/userSignup.js
+++ frontend/src/hooks/userSignup.js
@@ -17,6 +17,29 @@ const userSignup = () => {
       gender,
     });
     if (!success) return;
+    setLoading(true);
+    try {
+      const res = fetch("http://localhost:5000/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          fullName,
+          username,
+          password,
+          confirmPassword,
+          gender,
+        }),
+      });
+
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
 };
 
@@ -32,4 +55,15 @@ function handleInputErrors({
   if (!fullName || !username || !password || !confirmPassword || !gender) {
     toast.error("Please fill in all fields");
   }
+
+  if (password !== confirmPassword) {
+    toast.error("Passwords do not match");
+    return false;
+  }
+  if (password.length < 6) {
+    toast.error("Password must be at least 6 characters long");
+    return false;
+  }
+
+  return true;
 }
